db9165ab717bfa94334a445db3988851aa12fb47
chevezjulio <chevezjulio542@gmail.com>
2026-05-26 15:50:31 -0600

    Ahora si subiendo todo el proyecto unificado

---
+++ Fundamentos-de-Python/2_condicionales/condicionales.py
@@ -0,0 +1,9 @@
+print("Escribe tu nombre:")
+nombre = input()
+nnombre = int(int())
+
+if nombre == "Leo" or nombre == "Adalberto":
+    print("Me gusta tu nombre")
+else: 
+    print("Que nombre tan extraño")
+
---
+++ Fundamentos-de-Python/5_tuplas/tuplas.py
@@ -0,0 +1,10 @@
+tupla = ("Rodrigo", "Juan")
+
+videojuegos = []
+
+#Nombre, Genero, Año
+videojuegos.append(("FC 26", "FUTBOL", 1993))
+videojuegos.append(("FORZA HORIZON 5", "ACCION", 2012))
+
+print(videojuegos)
+
