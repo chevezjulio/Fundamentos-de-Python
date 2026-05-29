c5176c57e1aa85ab9bf8db98caf471a7bc56928d
chevezjulio <chevezjulio542@gmail.com>
2026-05-28 18:31:29 -0600

    Estructura completa: agregadas carpetas hasta diccionarios y nuevo codigo

--- Fundamentos-de-Python/02_condicionales/condicionales.py
+++
@@ -1,9 +0,0 @@
-print("Escribe tu nombre:")
-nombre = input()
-nnombre = int(int())
-
-if nombre == "Julio" or nombre == "Alexander":
-    print("Me gusta tu nombre")
-else: 
-    print("Que nombre tan extraño")
-
---
+++ Fundamentos-de-Python/11_diccionarios/diccionarios.py
@@ -0,0 +1,15 @@
+#Lista de elementos llave:valor
+#Arreglos asociativos
+
+persona = {"nombre": "Rodrigo",
+           "edad": 800,
+           "apellido": "Montemayor"}
+
+persona["apodo"] = "Ringa Tech"
+
+#print(persona.items())
+
+for key, value in persona.items():
+    print(f"la llave {key} tiene el valor { value}")
+
+#for key, value in persona.items():
